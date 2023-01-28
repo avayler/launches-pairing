91209a4dcd692cf3cc12e8ddcf75d9199158364c
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-28 23:31:33 +0000

    Update classNames.test.ts

--- __tests__/classNames.test.ts
+++ __tests__/classNames.test.ts
@@ -1,7 +1,6 @@
-// import React from 'react';
 import classNames from '../utilities/classNames';
 
-describe('classNames', () => {
+describe('classNames utility function', () => {
   it('Verify that className string is constructed and is valid', () => {
     const className = classNames(' foo', 'bar ', ' baz ', '', ' ', null, undefined);
     expect(className).toEqual('foo bar baz');
