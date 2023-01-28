6210b66f91dbd4d27b61feb5d8f178363e93f6e0
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-28 23:31:11 +0000

    Fixed incorrect length issue

--- utilities/randomString.ts
+++ utilities/randomString.ts
@@ -1,3 +1,3 @@
 // Generate a random string of a determined length
-const randomString = (length = 8) => (Math.random() + 1).toString(36).substring(length);
+const randomString = (length = 8) => (Math.random() + 1).toString(36).substring(0, length);
 export default randomString;
