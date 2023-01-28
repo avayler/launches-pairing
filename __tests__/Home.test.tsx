38ebb7db5db31ed3a45c85ec6986ff5de8ef4bb1
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-28 23:31:35 +0000

    Update Home.test.tsx

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -4,21 +4,19 @@
 
 require('dotenv').config();
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
-import axios from 'axios';
+import { render } from '@testing-library/react';
 import launchData from '../data/launches-test.json';
 import Home from '../pages';
 
 const { SPACEX_API_BASEURL, SPACEX_API_LAUNCHES_SLUG } = process.env;
 const url = `${SPACEX_API_BASEURL}${SPACEX_API_LAUNCHES_SLUG}/query`;
-const axiosGet = jest.spyOn(axios, 'get');
 
-describe('Home Page', () => {
+describe('Home page component', () => {
   it('Verify that Home page renders a list of Card components', async () => {
     const { container } = render(<Home preloadData={launchData} apiUrl={url} />);
-    // expect(container).toMatchSnapshot();
     const cards = container.querySelectorAll('.card');
+
+    expect(container).toMatchSnapshot();
     expect(cards).toHaveLength(10);
-    // await waitFor(() => expect(axiosGet).toHaveBeenCalledTimes(1));
   });
 });
