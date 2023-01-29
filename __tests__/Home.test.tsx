3a18d675a6c7a25d78e3ad3c55f646d2719da6c2
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-29 00:41:16 +0000

    Updated Home.test with test for rendered Cards

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -3,6 +3,7 @@
  */
 
 require('dotenv').config();
+import '@testing-library/jest-dom';
 import React from 'react';
 import { render } from '@testing-library/react';
 import launchData from '../data/launches-test.json';
@@ -18,5 +19,28 @@ describe('Home page component', () => {
 
     expect(container).toMatchSnapshot();
     expect(cards).toHaveLength(launchData.length);
+
+    for (let i = 0; i < cards.length; i++) {
+      const { name, date, core, image_url, success, payloads, failureReason } = launchData[i] as Launch;
+      const card = cards[i];
+      const cardImage = card.querySelector('img');
+      const cardBody = card.querySelector('.card_body');
+      const cardFooter = card.querySelector('.card_footer');
+
+      expect(cardImage).not.toBeNull();
+      // expect(cardImage.getAttribute('src')).toMatch(encodeURI(image_url));
+      expect(cardImage).toHaveAttribute('alt', name);
+
+      expect(cardBody).not.toBeNull();
+      // expect(cardBody).toContain(name.toUpperCase());
+      // expect(cardBody).toContain(date);
+      expect(cardFooter).not.toBeNull();
+      // expect(cardFooter).toContain(success ? 'Successfull' : 'Failure');
+
+      // for (let j = 0; j < payloads.length; j++) {
+      //   expect(card).toContain(payloads[j].id);
+      //   expect(card).toContain(payloads[j].type);
+      // }
+    }
   });
 });
