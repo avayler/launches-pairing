7f57b128e544f7bcde54db0e7d59bd4924144ff0
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-29 00:40:38 +0000

    Update mapToJSX.test.tsx
    Removed the rendering of the output

--- __tests__/mapToJSX.test.tsx
+++ __tests__/mapToJSX.test.tsx
@@ -1,25 +1,19 @@
-/**
- * @jest-environment jsdom
- */
-
-import React from 'react';
-import { render } from '@testing-library/react';
 import launchData from '../data/launches-test.json';
 import Card from '../components/Card';
 import mapToJSX from '../utilities/mapToJSX';
 
 describe('mapToJSX utility function', () => {
-  it('Verify that function renders a list of Card components', async () => {
+  it('Verify that function returns an array of react elements', async () => {
     const elements = mapToJSX(launchData, Card);
-    const { container } = render(<>{elements}</>);
-    const cards = container.querySelectorAll('.card');
 
     expect(elements).toHaveLength(launchData.length);
-    expect(cards).toHaveLength(launchData.length);
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      const launch = launchData[i];
 
-    for (let i = 0; i < cards.length; i++) {
-      const { name, date, core, image_url, success, payloads, failureReason } = launchData[i] as Launch;
-      const card = cards[i];
+      expect(element.type).toBeInstanceOf(Function);
+      expect(element.type.name).toBe('Card');
+      expect(element.props).toEqual(launch);
     }
   });
 });
