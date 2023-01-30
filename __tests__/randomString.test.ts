0389dd4cf6d9e44664b76d323193ac320ecc6abd
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-30 23:04:40 +0000

    Update test description

--- __tests__/randomString.test.ts
+++ __tests__/randomString.test.ts
@@ -1,9 +1,11 @@
 import randomString from '../utilities/randomString';
 
 describe('randomString utility function', () => {
-  it('Verify that randomString value is valid', () => {
+  it('Verify that random value is of the correct length and is a string', () => {
     const length = 8;
     const value = randomString(length);
+
+    expect(typeof value).toBe('string');
     expect(value).toHaveLength(length);
   });
 });
