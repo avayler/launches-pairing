77f7adb431d852720a02668ec91b99573e102a15
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-30 23:05:10 +0000

    Update test to check for correct component name

--- __tests__/mapToJSX.test.tsx
+++ __tests__/mapToJSX.test.tsx
@@ -1,18 +1,19 @@
 import launchData from '../data/launches-test.json';
-import Card from '../components/Card';
+import LaunchCard from '../components/Launch';
 import mapToJSX from '../utilities/mapToJSX';
 
 describe('mapToJSX utility function', () => {
   it('Verify that function returns an array of react elements', async () => {
-    const elements = mapToJSX(launchData, Card);
+    const elements = mapToJSX(launchData, LaunchCard);
 
     expect(elements).toHaveLength(launchData.length);
+    // Verify that each element is a react element with the correct props and type
     for (let i = 0; i < elements.length; i++) {
       const element = elements[i];
       const launch = launchData[i];
 
       expect(element.type).toBeInstanceOf(Function);
-      expect(element.type.name).toBe('Card');
+      expect(element.type.name).toBe('Launch');
       expect(element.props).toEqual(launch);
     }
   });
