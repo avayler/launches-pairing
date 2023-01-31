9d9224ac357e65343fc868a6dd83af9004740585
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-31 12:19:15 +0000

    Reduced icon sizes to 24px

--- components/icons/Failure.tsx
+++ components/icons/Failure.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Failure() {
   return (
-    <svg width="32" height="32" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M22 30H26V34H22V30ZM22 14H26V26H22V14ZM23.98 4C12.94 4 4 12.96 4 24C4 35.04 12.94 44 23.98 44C35.04 44 44 35.04 44 24C44 12.96 35.04 4 23.98 4ZM24 40C15.16 40 8 32.84 8 24C8 15.16 15.16 8 24 8C32.84 8 40 15.16 40 24C40 32.84 32.84 40 24 40Z"
         fill="currentColor"
