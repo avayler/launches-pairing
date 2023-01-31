83f15af1ed9e6aa4d93f1600775f54f4893fcd77
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-31 11:56:27 +0000

    Reduced icon sizes to 32px

--- components/icons/Close.tsx
+++ components/icons/Close.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Close() {
   return (
-    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="32" height="32" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clipPath="url(#clip0_22_91)">
         <path
           d="M38 12.82L35.18 10L24 21.18L12.82 10L10 12.82L21.18 24L10 35.18L12.82 38L24 26.82L35.18 38L38 35.18L26.82 24L38 12.82Z"
